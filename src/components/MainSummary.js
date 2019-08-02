8e13237f02853a187f27dc4e99ec98c546252daa
Chau Tran <cmtran7393@gmail.com>
2019-08-01 21:45:15 -0400

    refactored into components

---
+++ src/components/MainSummary.js
@@ -0,0 +1,38 @@
+import React , {Component} from 'react';
+import SummaryOptions from './SummaryOptions';
+import SummaryTotal from './SummaryTotal';
+
+export default class MainSummary extends Component {
+
+    static defaultProps = {
+        selected: '',
+    }
+
+    render(){
+
+        const {selected} = this.props;
+
+        const summary = Object.keys(selected)
+          .map((key, index) => {
+
+           return  (
+                <SummaryOptions
+                    key={index}
+                    summaryKey={key}
+                    selected={selected}
+                />
+            );
+            
+          });
+
+        return (
+            <section className="main__summary">
+                <h3>NEW GREENLEAF 2018</h3>
+                {summary}
+                <SummaryTotal 
+                    selected={selected}
+                />                
+            </section>
+        );
+    }
+}
